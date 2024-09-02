68dc0917af5ecf46502a3c2f419bfcdf815e5d05
NACIMSAAFI <7715@holbertonstudents.com>
2024-09-02 17:42:37 +0100

    Fix style

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-"""You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
 
 Write a method that determines if all the boxes can be opened.
-
 """
+
+from collections import deque
 
 
 def canUnlockAll(boxes):
@@ -12,7 +14,8 @@ def canUnlockAll(boxes):
     Determines if all boxes can be unlocked.
 
     Parameters:
-        boxes (list of list of int): A list of lists where each sublist contains keys to other boxes.
+        boxes (list of list of int): A list of lists where each sublist
+        contains keys to other boxes.
 
     Returns:
         bool: True if all boxes can be opened, else False.
@@ -20,15 +23,14 @@ def canUnlockAll(boxes):
     n = len(boxes)  # Number of boxes
     unlocked = [False] * n  # Keep track of which boxes are unlocked
     unlocked[0] = True  # We can always open the first box
-    queue = [0]  # Start with the first box in the queue
+    queue = deque([0])  # Start with the first box in the queue
 
     while queue:
-        box = queue.pop(0)  # Get the current box
+        box = queue.popleft()  # Get the current box
         # Iterate over all the keys in the current box
         for key in boxes[box]:
-            if (
-                key < n and not unlocked[key]
-            ):  # Check if the box to unlock is within range and not yet unlocked
+            if key < n and not unlocked[key]:
+                # Check if the box to unlock is within range and not yet unlocked
                 unlocked[key] = True  # Mark the box as unlocked
                 queue.append(key)  # Add the newly unlocked box to the queue
 
