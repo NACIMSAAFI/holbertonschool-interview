dfd238efa329fff20f0218b7bfde582379f0b043
NACIMSAAFI <7715@holbertonstudents.com>
2024-09-22 23:32:53 +0100

    Task 0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -8,11 +8,12 @@ def minOperations(n):
         return 0
 
     operations = 0
-    current = 1
-
-    for i in range(2, n + 1):
+    for i in range(2, int(n**0.5) + 1):
         while n % i == 0:
             operations += i
             n //= i
+
+    if n > 1:
+        operations += n
 
     return operations
