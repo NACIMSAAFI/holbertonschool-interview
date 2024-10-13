c2d631040bfaa5be70e44587da501238a8baec34
NACIMSAAFI <7715@holbertonstudents.com>
2024-10-13 23:48:23 +0100

    Task 0

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,80 +1,58 @@
 #!/usr/bin/python3
-
 """
-Log parsing algorithm that takes logs from stdin
-and shows the file size and the number of appearances of each
-type of error.
+Module that parses a log and prints stats to stdout.
 """
 
-import re
-import sys
+from sys import stdin
 
 
-def main():
-    """Main function"""
-    error_dict = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0,
+}
 
-    num_lines = 0
-    file_size = 0
-
-    file_size_regex = r"\d{1,4}$"
-    error_regex = r"(?<=\s)\d{3}(?=\s\d{1,4}$)"
-    log_entry_regex = re.compile(r"""
-            .*\s-\s
-            \[
-            \d{4}-\d{1,2}-\d{1,2}
-            \s
-            \d{1,2}:\d{1,2}:\d{1,2}
-            \.\d{6}
-            \]\s
-            "GET\s\/projects\/260\sHTTP\/1\.1"
-            \s\d{3}\s
-            \d{1,4}
-        """, re.VERBOSE)
-    data = sys.stdin.read()
-
-    if data.strip() == "":
-        print("File size: 0")
-        return
-
-    lines = data.splitlines()
-
-    try:
-        for line in lines:
-            if re.match(log_entry_regex, line):
-                num_lines += 1
-                file_size += int(re.search(file_size_regex, line).group())
-                error_code = re.search(error_regex, line).group()
-                error_dict[error_code] += 1
-
-                if num_lines == 10:
-                    print_stats(file_size, error_dict)
-                    num_lines = 0
-
-        if num_lines > 0:
-            print_stats(file_size, error_dict)
-
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        print_stats(file_size, error_dict)
+size = 0
 
 
-def print_stats(file_size, error_dict):
-    """Prints the accumulated statistics."""
-    print(f"File size: {file_size}")
-    for code, count in sorted(error_dict.items()):
-        if count > 0:
-            print(f"{code}: {count}")
+def print_stats():
+    """Prints the accumulated logs."""
+    print("File size: {}".format(size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status] > 0:
+            print("{}: {}".format(status, status_codes[status]))
 
 
 if __name__ == "__main__":
-    main()
+    count = 0
+    try:
+        for line in stdin:
+            items = line.split()
+            if len(items) < 2:
+                continue
+
+            try:
+                size += int(items[-1])
+                status_code = items[-2]
+
+                if status_code in status_codes:
+                    status_codes[status_code] += 1
+            except ValueError:
+
+                continue
+
+            if count == 9:
+                print_stats()
+                count = -1
+            count += 1
+
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+
+    print_stats()
